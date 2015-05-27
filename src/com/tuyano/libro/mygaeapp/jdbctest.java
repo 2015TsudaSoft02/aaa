d82290032c649a2133be56246184a82fa6df8fec
g13956ym <g13956ym+git@gm.tsuda.ac.jp>
2015-05-27 21:03:05 +0900

    ok

---
+++ src/com/tuyano/libro/mygaeapp/jdbctest.java
@@ -0,0 +1,15 @@
+package com.tuyano.libro.mygaeapp;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author 
+ */
+class jdbctest {
+
+    static Connection getConnection() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
