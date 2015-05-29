6c3e1f476a3b931bb02734326a0fdacca97b868f
g13956ym <g13956ym+git@gm.tsuda.ac.jp>
2015-05-29 19:10:34 +0900

    たくさん変更した！

--- src/com/tuyano/libro/mygaeapp/PMF.java
+++ src/com/tuyano/libro/mygaeapp/PMF.java
@@ -4,12 +4,12 @@ import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManagerFactory;
 
 public class PMF {
-   private static final PersistenceManagerFactory factory = 
-       JDOHelper.getPersistenceManagerFactory("transactions-optional");
-    
-   private PMF(){}
-    
-   public static PersistenceManagerFactory get(){
-       return factory;
-   }
+    private static final PersistenceManagerFactory factory = 
+        JDOHelper.getPersistenceManagerFactory("transactions-optional");
+     
+    private PMF(){}
+     
+    public static PersistenceManagerFactory get(){
+        return factory;
+    }
 }
